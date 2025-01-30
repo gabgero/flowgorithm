dc0ef05c8ee6bfba60758204a221f25b892533dd
gabgero <161519809+gabgero@users.noreply.github.com>
2025-01-30 20:27:44 +0100

    Add files via upload

---
+++ flowgoritmh.py
@@ -0,0 +1,14 @@
+﻿print("Ciao!")
+print("questa è una prova con flowgorithm")
+parola = input()
+if parola == "marra":
+    oioioi = input()
+    if oioioi == "cash":
+        print("Fine del programma")
+        print("ascolta l'album di marracash")
+    else:
+        print("DROGATO")
+else:
+    print("DROGATO")
+print("scrivi fine")
+t = input()
